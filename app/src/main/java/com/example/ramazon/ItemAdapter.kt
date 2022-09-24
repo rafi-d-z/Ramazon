01129a15e816976c7a6835490d229432d4c66a8b
rafi-d-z <rafidizia@gmail.com>
2022-09-23 23:16:08 -0400

    commit 1

---
+++ app/src/main/java/com/example/ramazon/ItemAdapter.kt
@@ -0,0 +1,44 @@
+package com.example.ramazon
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class ItemAdapter(private val WishlistItems: ArrayList<WishlistItem>) :RecyclerView.Adapter<ItemAdapter.ViewHolder>(){
+    class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
+
+        val itemNameView: TextView
+        val linkView: TextView
+        val priceView: TextView
+
+        init {
+            itemNameView = itemView.findViewById(R.id.itemName)
+            linkView = itemView.findViewById(R.id.link)
+            priceView = itemView.findViewById(R.id.Price)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val context = parent.context
+        val inflater = LayoutInflater.from(context)
+        // Inflate the custom layout
+        val contactView = inflater.inflate(R.layout.wishlist_item, parent, false)
+        // Return a new holder instance
+        return ViewHolder(contactView)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val wishlistItem = WishlistItems[position]
+
+        holder.itemNameView.text = wishlistItem.itemName
+        holder.priceView.text = wishlistItem.itemPrice.toString()
+        holder.linkView.text = wishlistItem.itemLink
+    }
+
+    override fun getItemCount(): Int {
+        return 1
+    }
+
+}
--- app/src/main/java/com/example/ramazon/MainActivity.kt
+++ app/src/main/java/com/example/ramazon/MainActivity.kt
@@ -2,18 +2,39 @@ package com.example.ramazon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import java.net.URL
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlin.collections.ArrayList
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val wishlistItems = ArrayList<WishlistItem>()
+
+        val itemsRv = findViewById<RecyclerView>(R.id.recylerview)
+        val adapter = ItemAdapter(wishlistItems)
+
+        itemsRv.adapter = adapter
+
+        itemsRv.layoutManager = LinearLayoutManager(this)
+
+        findViewById<Button>(R.id.addBtn).setOnClickListener {
+            val itemNameInput = findViewById<EditText>(R.id.itemNameBox).text.toString()
+            val itemPriceInput = findViewById<EditText>(R.id.priceBox).text
+            val itemLinkInput = findViewById<EditText>(R.id.linkBox).text.toString()
+
+            val newItem = WishlistItem(itemNameInput,itemLinkInput,itemPriceInput)
+            wishlistItems.add(newItem)
+        }
     }
 }
 
 class WishlistItem(
     val itemName: String,
-    val itemLink: URL,
-    val itemPrice: Double,){
-
-}
+    val itemLink: String,
+    val itemPrice: Editable,)
